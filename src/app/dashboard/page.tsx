faf0777ae829468524c7731e08de32da648dacae
RaulLiberato <ctt.raul.liberato@gmail.com>
2025-05-28 17:28:31 -0300

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import { authClient } from "@/lib/auth-client";
 import { headers } from "next/headers";
 import SignOutButton from "./components/sign-out-button";
 import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -10,6 +13,12 @@ const DashboardPage = async () => {
   });
   if (!session?.user) {
     redirect("/authentication");
+  }
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return (
