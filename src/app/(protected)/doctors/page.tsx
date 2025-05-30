a1f36cf10f9d729fa2bde9c69ff9c15ea4801dcf
RaulLiberato <ctt.raul.liberato@gmail.com>
2025-05-29 22:31:48 -0300

    feat: ensure doctors are listed on doctors page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -11,7 +11,10 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import AddDoctorButton from "./_components/add-doctor-button";
-
+import { doctorsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { db } from "@/db";
+import DoctorCard from "./_components/doctor_card";
 const DoctorsPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -22,6 +25,10 @@ const DoctorsPage = async () => {
   if (!session.user.clinic) {
     redirect("/clinic-form");
   }
+
+  const doctors = await db.query.doctorsTable.findMany({
+    where: eq(doctorsTable.clinicId, session.user.clinic.id),
+  });
 
   return (
     <PageContainer>
@@ -35,7 +42,11 @@ const DoctorsPage = async () => {
         </PageActions>
       </PageHeader>
       <PageContent>
-        <h1>Médicos</h1>
+        <div className="grid grid-cols-3 gap-6">
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+          ))}
+        </div>
       </PageContent>
     </PageContainer>
   );
